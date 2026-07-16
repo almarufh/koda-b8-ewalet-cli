fc5abb5b5fdff493ff8064e6595746589961dd7b
almarufh_ <hidayatmaruf99@gmail.com>
2026-07-16 23:34:52 +0800

    fix: move position folder

--- internal/database/struct.go
+++ internal/database/struct.go
@@ -1,12 +1,26 @@
-package types
+package database
 
 import (
 	"time"
 )
 
+type DataUser struct {
+	ID          int64
+	Username    string
+	Email       string
+	Password    string
+	Pin         *string
+	Balance     int64
+	FirstName   string
+	LastName    string
+	DateOfBirth time.Time
+	Address     *string
+	Mother      *string
+	Code        string
+}
+
 type User struct {
-	ID        int64     `json:"id"`
-	Username  string    `json:"username"`
+	ID        string    `json:"id"`
 	Email     string    `json:"email"`
 	Password  string    `json:"password"`
 	Pin       *string   `json:"pin,omitempty"`
@@ -15,9 +29,8 @@ type User struct {
 }
 
 type Balance struct {
-	ID        int64     `json:"id"`
+	ID        string    `json:"id"`
 	Balance   int64     `json:"balance"`
-	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
 
@@ -28,11 +41,10 @@ type Profile struct {
 	DateOfBirth time.Time `json:"date_of_birth"`
 	Address     *string   `json:"address,omitempty"`
 	Mother      *string   `json:"mother,omitempty"`
-	CreatedAt   time.Time `json:"created_at"`
 	UpdatedAt   time.Time `json:"updated_at"`
 }
 
-type OTP struct {
+type Otp struct {
 	ID        int64     `json:"id"`
 	Code      string    `json:"code"`
 	Used      bool      `json:"used"`
