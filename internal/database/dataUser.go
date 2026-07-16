4422be77bee77df09f1340c4e2e1e1df7b7b96f2
almarufh_ <hidayatmaruf99@gmail.com>
2026-07-16 18:18:56 +0800

    feat: find user by username

--- internal/database/dataUser.go
+++ internal/database/dataUser.go
@@ -21,3 +21,17 @@ func FindUserById(key int64) (*User, error) {
 	}
 	return &user, nil
 }
+
+func FindUserByUsername(key string) (*User, error) {
+	var user User
+	query := `SELECT "id", "username", "email", "password", "pin" FROM "users" WHERE "username" = $1;`
+	conn, res := GetSinggle(query, key)
+
+	err := res.Scan(&user.ID, &user.Username, &user.Email, &user.Password, &user.Pin)
+	defer conn.Close(context.Background())
+
+	if err != nil {
+		fmt.Println("Gagal collect Table")
+	}
+	return &user, nil
+}
