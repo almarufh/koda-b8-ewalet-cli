14d825fe5bd097c69105484716030f1c8ea7f46c
almarufh_ <hidayatmaruf99@gmail.com>
2026-07-16 23:28:03 +0800

    feat: create table otp

--- migrations/000004_create_table_users_profiles.up.sql
+++ migrations/000004_create_table_users_profiles.up.sql
@@ -1,4 +1,4 @@
 CREATE TABLE "users_profiles" (
-    "id_user" VARCHAR(20) REFERENCES "users"("id"),
+    "id_user" VARCHAR(13) REFERENCES "users"("id"),
     "id_profile" bigint  REFERENCES "profiles"("id")
 );
