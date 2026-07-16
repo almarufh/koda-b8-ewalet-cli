0c82b0e2e5e30640fcc120c23f627ce2fa05c734
almarufh_ <hidayatmaruf99@gmail.com>
2026-07-16 19:32:10 +0800

    feat: find Balance ByUserName and Delete func find by user by id

--- internal/database/dataUser.go
+++ internal/database/dataUser.go
@@ -7,21 +7,6 @@ import (
 
 // USER
 
-func FindUserById(key int64) (*User, error) {
-	var user User
-	query := `SELECT "id", "username", "email", "password", "pin" FROM "users" WHERE "id" = $1;`
-	conn, res := GetSinggle(query, key)
-
-	err := res.Scan(&user.ID, &user.Username, &user.Email, &user.Password, &user.Pin)
-
-	defer conn.Close(context.Background())
-
-	if err != nil {
-		fmt.Println("Gagal collect Table")
-	}
-	return &user, nil
-}
-
 func FindUserByUsername(key string) (*User, error) {
 	var user User
 	query := `SELECT "id", "username", "email", "password", "pin" FROM "users" WHERE "username" = $1;`
@@ -32,6 +17,27 @@ func FindUserByUsername(key string) (*User, error) {
 
 	if err != nil {
 		fmt.Println("Gagal collect Table")
+		panic(err.Error())
 	}
 	return &user, nil
+}
+
+// BALANCE
+func FindBalanceByUsername(key string) (*Balance, error) {
+	var balance Balance
+	query := `
+		SELECT "b"."balance" FROM "users" "u"
+		JOIN "users_balances" "ub" ON "ub"."id_user" = "u"."username"
+		JOIN "balances" "b" ON "b"."id" = "ub"."id_balance"
+		WHERE "username" = $1;`
+	conn, res := GetSinggle(query, key)
+
+	err := res.Scan(&balance.Balance)
+	defer conn.Close(context.Background())
+
+	if err != nil {
+		fmt.Println("Gagal collect Table")
+		panic(err.Error())
+	}
+	return &balance, nil
 }
